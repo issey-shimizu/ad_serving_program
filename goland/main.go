c31cb23dfd9ef34f14d92ffc3dd00f32d2f69a7c
shimizu-issei <isseichuo@gmail.com>
2023-02-06 20:03:50 +0900

    fix

--- goland/main.go
+++ goland/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -11,6 +12,7 @@ import (
 	"src/handler"
 	"src/repository"
 
+	"github.com/go-redis/redis"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
 )
@@ -20,6 +22,11 @@ const tmplPath = "src/template/"
 var db *sqlx.DB
 var e = createMux()
 
+type Request struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
+}
+
 func main() {
 
 	db = connectDB()
@@ -28,6 +35,82 @@ func main() {
 	e.GET("/impression/:id", handler.Impression)
 	e.GET("/click/:id", handler.ShowCookie)
 	e.GET("/conversion/:id", handler.Conversion)
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World")
+	})
+
+	client := redis.NewClient(&redis.Options{
+		Addr:     "redis:6379",
+		Password: "",
+		DB:       0,
+	})
+
+	e.GET("/get", func(c echo.Context) error {
+		key := "advertise"
+		// Set
+		/*
+			for field, val := range map[string]string{"name": "shimizu", "age": "26"} {
+				fmt.Println("Inserting", "field:", field, "val:", val)
+				err := client.HSet(key, field, val).Err()
+				if err != nil {
+					fmt.Println("redis.Client.HSet Error:", err)
+				}
+			}
+		*/
+
+		// Get
+		// HGet(key, field string) *StringCmd
+		hgetVal, err := client.HGet(key, "image_url").Result()
+		if err != nil {
+			fmt.Println("redis.Client.HGet Error:", err)
+		}
+		fmt.Println(hgetVal)
+
+		return nil
+	})
+
+	/*
+		e.GET("/get", func(c echo.Context) error {
+			fmt.Println("Go Redis Connection Test")
+
+			ping, err := client.Ping().Result()
+			fmt.Println(ping, err)
+
+			key := c.QueryParam("key")
+			val, err := client.Get(key).Result()
+
+			if err != nil {
+				return err
+			}
+
+			data := new(Request)
+			data.Key = key
+			data.Value = val
+			log.Println("keyの値")
+			log.Println(data.Key)
+			log.Println("Valueの値")
+			log.Println(data.Value)
+
+			return c.JSON(http.StatusOK, data)
+		})
+
+		e.POST("/set", func(c echo.Context) error {
+			key := c.FormValue("key")
+			value := c.FormValue("value")
+			err := client.Set(key, value, 0).Err()
+
+			if err != nil {
+				return err
+			}
+
+			data := new(Request)
+			data.Key = key
+			data.Value = value
+
+			return c.JSON(http.StatusOK, data)
+		})
+	*/
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
